c79c471731c10b22caf0894dbfc9fac047557417
Uriel Carbajal G <89610193+UrielCG1@users.noreply.github.com>
2024-03-26 14:47:19 -0600

    header Responsive
    Se añadío el Boostrap y lo más importante, ya pude hacer el header responsive, para que se apareciera una Sidebar para que el usuario pueda acceder a los diferentes "Enlaces" de la página

--- myScript.js
+++ myScript.js
@@ -1,8 +1,31 @@
-function toggleMe() {
-  var x = document.getElementById("demo");
+/*const menuIcon = document.querySelector('.menu-icon');
+const enlaces = document.querySelector('.enlaces');
+const navbar = document.querySelector('.navbar');
 
-  x.style.display = x.style.display === "none" ? "block" : "none";
+menuIcon.addEventListener('click', () => {
+  navbar.enlaces.style.display = (navbar.enlaces.style.display === 'flex') ? 'none ' : 'none';
+});
+*/
+/*const menuIcon = document.getElementById('menu-icon');
+const enlaces = document.getElementById('enlaces');
 
-  var buttonText = x.style.display === "none" ? "Mostrar!" : "ocultar!";
-  document.getElementById("toggleButton").textContent = buttonText;
+menuIcon.addEventListener('click', () => {
+    enlaces.style.display = (enlaces.style.display === 'flex') ? 'none' : 'column';
+});*/
+
+
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = 'flex';
+  setTimeout(() => {
+      sidebar.style.transform = 'translateX(0)'; 
+  }, 10);
+}
+
+function closeSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.transform = 'translateX(100%)'; 
+  setTimeout(() => {
+      sidebar.style.display = 'none';
+  }, 300);
 }
